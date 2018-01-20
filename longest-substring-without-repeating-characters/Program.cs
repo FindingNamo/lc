3a7e2bed08eca0b0a1f304945d21a4f3b09db134
FindingNamo <davidminhnamngo@gmail.com>
2018-01-19 23:18:30 -0800

    Solve it

--- longest-substring-without-repeating-characters/Program.cs
+++ longest-substring-without-repeating-characters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace longest_substring_without_repeating_characters
 {
@@ -6,7 +7,39 @@ namespace longest_substring_without_repeating_characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{LengthOfLongestSubstring("pwwkew")}");
+        }
+
+        // enumerate letters from index = 0
+        // save letters in dictionary letter/index
+        // when find existing letter reset count and dictionary and start at index of repeated letter
+        // do until end of string
+        public static int LengthOfLongestSubstring(string s) 
+        {
+            int index = 0;
+            int count = 0;
+            int max = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            while(index < s.Length)
+            {
+                if(!map.ContainsKey(s[index]))
+                {
+                    map.Add(s[index], index);
+                    count++;
+                    if (count > max)
+                        max = count;
+                }
+                else 
+                {
+                    count = 0;
+                    index = map[s[index]];
+                    map = new Dictionary<char, int>();
+                }
+
+                index++;
+            }
+            
+            return max;
         }
     }
 }
