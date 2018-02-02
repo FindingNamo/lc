bfde5c7a98e5d606a949c44c3ca0f69ca2dde187
FindingNamo <davidminhnamngo@gmail.com>
2018-02-01 22:25:08 -0800

    Basic solution to reverse integer

---
+++ reverse-integer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace reverse_integer {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ($"{new Solution().Reverse(1534236469)}");
+        }
+    }
+
+    public class Solution {
+        public int Reverse (int x) {
+            int result = 0;
+            try {
+                do {
+                    // increment by least significant digit
+                    result = checked (result + (x % 10));
+
+                    // get ready to get next least significant digit
+                    x = x / 10;
+
+                    // if we're not done, prep result for next round of addition
+                    if (x != 0)
+                        result = checked (result * 10);
+                } while (x != 0);
+            } catch (OverflowException e) {
+                return 0;
+            }
+
+            return result;
+        }
+    }
+}
