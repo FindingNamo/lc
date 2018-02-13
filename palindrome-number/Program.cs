662b195fdca9f7b347c609c3bfeff55704c4e892
FindingNamo <davidminhnamngo@gmail.com>
2018-02-12 22:19:14 -0800

    Basic solution to palindrome number without additional space needed

---
+++ palindrome-number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace palindrome_number
+{
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine (new Solution().IsPalindrome(-2147483648));
+        }
+    }
+
+    public class Solution {
+        public bool IsPalindrome (int x) {
+            if (x < 0)
+                return false;
+
+            var m = GetMagnitude (x);
+
+            for (int i = 0; i <= m/2; i++) {
+                var leftMag = (int)Math.Pow(10,m-i);
+                var leftMost = (x / leftMag) % 10;
+                var rightMag = (int)Math.Pow(10,i);
+                var rightMost = (x / rightMag) % 10;
+
+                if (leftMost != rightMost)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private int GetMagnitude (int x) {
+            var result = Math.Log ((double)x, 10d);
+            return (int)result;
+        }
+    }
+}
---
+++ string-to-integer-atoi/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace string_to_integer_atoi {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ($"{new Solution().MyAtoi("+-2")}");
+        }
+    }
+
+    public class Solution {
+        public int MyAtoi (string str) {
+
+            if (string.IsNullOrWhiteSpace (str))
+                return 0;
+
+            if (IsBeeEssCase(str))
+                return 0;
+
+            str.Trim();
+
+            bool isNeg = str.Length > 1 && str[0] == '-';
+
+            if(isNeg)
+                str = str.Substring(1);
+
+            str = TrimNonDigitPrefixes(str);
+
+            var result = FirstNumber(str);
+
+            try {
+                return isNeg ? result * -1 : result;
+            } catch {
+                return 0;
+            }
+        }
+
+        private bool IsBeeEssCase(string s){
+            if(s.StartsWith("+-"))
+                return true;
+
+            return false;
+        }
+
+        private string TrimNonDigitPrefixes(string s){
+            int temp;
+            int index = -1;
+            for(int i = 0; i < s.Length; i++){
+                if(Int32.TryParse(s[i].ToString(), out temp)){
+                    index = i;
+                    break;
+                }
+                    
+            }
+
+            return index < 0 ? string.Empty :  s.Substring(index);
+        }
+
+        private int FirstNumber(string s){
+            var result = 0;
+            for(int i = 0; i < s.Length; i++){
+                int curDigit;
+                if(Int32.TryParse(s[i].ToString(), out curDigit)){
+                    result = result * 10 + curDigit;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+
+            return result;
+        }
+    }
+}
