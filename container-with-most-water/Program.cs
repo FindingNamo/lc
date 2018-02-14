b92863261bc19cd2c5daf0eb488a29fb892f3f07
FindingNamo <davidminhnamngo@gmail.com>
2018-02-13 22:08:12 -0800

    Brute force o(n^2) and o(n) solutions to max area

---
+++ container-with-most-water/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace container_with_most_water {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine (new Solution().MaxArea(new [] {1,2,3}));
+        }
+    }
+
+    public class Solution {
+        public int MaxArea (int[] height) {
+            int maxArea = 0;
+            /* Brute Force 
+            for(int i = 1; i < height.Length; i++){
+                for (int j = 0; j < i; j++){
+                    var minHeight = Math.Min(height[j], height[i]);
+                    var curArea = minHeight * (i - j);
+                    if (curArea > maxArea)
+                        maxArea = curArea;
+
+                }
+            }*/
+            int left = 0;
+            int right = height.Length - 1;
+            int curArea =0;
+            while(left<right){
+                curArea = Math.Min(height[left],height[right]) * (right - left);
+                if (curArea > maxArea)
+                    maxArea = curArea;
+                if(height[left] >= height[right])
+                    right--;
+                else
+                    left++;
+            }
+            
+
+            return maxArea;
+        }
+    }
+}
