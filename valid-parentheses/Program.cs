9411eadbd60557b90e4bbb5b4aab491cc7911446
FindingNamo <davidminhnamngo@gmail.com>
2018-02-25 21:58:46 -0800

    Solution to parentheses validation using stack

---
+++ valid-parentheses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace valid_parentheses {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+    public class Solution {
+
+        private Dictionary<char, char> parenthesesPairs = new Dictionary<char, char> { { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+        public bool IsValid (string s) {
+            return UsingStack (s);
+        }
+
+        private bool UsingStack (string s) {
+            Stack<char> openParentheses = new Stack<char> ();
+            foreach (char c in s) {
+                if (parenthesesPairs.ContainsKey (c)) {
+                    openParentheses.Push (c);
+                } else {
+                    if (openParentheses.Count == 0)
+                        return false;
+                    char lastOpened = openParentheses.Pop ();
+                    if (c != parenthesesPairs[lastOpened])
+                        return false;
+                }
+            }
+
+            if (openParentheses.Count != 0)
+                return false;
+
+            return true;
+        }
+    }
+}
